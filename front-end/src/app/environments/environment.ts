0d9c13d08bbd125aca609adadc920974c11d6c55
Tam Tran <nguyenvantrantam23052002@gmail.com>
2025-08-21 14:36:50 +0700

    sửa lại cấu hình build, sửa lại đường dẫn api

--- front-end/src/app/environments/environment.ts
+++ front-end/src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  socketUrl: 'http://chat-app-fullstack-production-efe2.up.railway.app', // hoặc domain thật nếu deploy
-  apiUrl: 'http://chat-app-fullstack-production-efe2.up.railway.app',
+  socketUrl: 'https://chat-app-fullstack-production-efe2.up.railway.app', // hoặc domain thật nếu deploy
+  apiUrl: 'https://chat-app-fullstack-production-efe2.up.railway.app',
 };
